7f5ea1892a3936e51d663c5cdd5b57cba345253e
VARUN SN <VARUN SN@LAPTOP-IS3DCPF8>
2022-10-14 16:17:49 +0530

    Add project files.

---
+++ GitHub/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GitHub
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
